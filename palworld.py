a6038c9a0fd0150f3f90d00567a3ad586b08c981
SomeDudRice <Lucasl197@nycstudents.net>
2026-03-26 10:17:01 -0400

    gotta

---
+++ palworld.py
@@ -0,0 +1,25 @@
+import json
+## Open the JSON file of pokemon data
+pokedex = open("./pokedex.json", encoding="utf8")
+## create variable "data" that represents the enitre pokedex list
+data = json.load(pokedex)
+# language = input("english, japanese, or chinese")
+
+# for index, data in enumerate(data):
+#  print(index ,":", data["name"][language])
+
+typeask = input ("type?")
+for index, data in enumerate(data):
+ if data["type"] == data[typeask]:
+  print(index ,":", data["name"]["english"])
+
+# Create a function that will take the data from the JSON file and you will iterate through the list of pokemon and print each pokemons name.
+
+# Add a language choice feature and print the pokemons name based on the user input
+
+# Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
+
+#Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
+
+#For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
+
